1b854fc7cc391d169c248bc1ede31782f557b3ca
Anoj98 <anojws@gmail.com>
2025-03-17 23:37:46 +0530

    add env package and file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
 import 'core/services/firebase_service.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Load env file
+  await dotenv.load(fileName: ".env");
+
   /// initializing firebase
   await FirebaseService.init();
+
   runApp(AiTrackingApp());
 }
